720e64bd2fa336c58bd84ad2ae34ec66a524b5c1
Stevan Kriss <stevan.kriss@duke.edu>
2021-08-30 12:43:27 -0400

    Set-up basic interface

---
+++ Blood_Calculator.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Blood Calculator")
+    print("Make a choice")
+    print("9 - Quit")
+    choice = input("Make a choice: ")
+    print(choice)
+    return choice
+
+
+interface()
