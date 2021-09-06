bd3e2fa282578698c338c48248b36d45cdb6bd37
Stevan Kriss <stevan.kriss@duke.edu>
2021-09-06 12:46:18 -0400

    Modularized code with subfuncitons

--- Dosing.py
+++ Dosing.py
@@ -5,7 +5,7 @@
              type of medical treatment or diagnostics.
 """
 
-def dose_amount():
+def Diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -14,6 +14,9 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+    
+def Weight():   
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -21,8 +24,14 @@ def dose_amount():
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
+    return weight,units
+    
+def Lb_Cnvrsn(weight,units): 
     if units == "lb":
-        weight = weight / 2.205
+        weight = weight / 2.205 
+    return weight
+        
+def Dosage_Calc(diagnosis,weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
@@ -31,5 +40,11 @@ def dose_amount():
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
+def Program_Driver():
+    diagnosis = Diagnosis()
+    weight,units = Weight()
+    weight = Lb_Cnvrsn(weight,units)
+    Dosage_Calc(diagnosis,weight)
+    
 if __name__ == '__main__':
-    dose_amount()
+    Program_Driver()
