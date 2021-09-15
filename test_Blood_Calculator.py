b3d8451ea41370fa4d9dfdf5630ca0382f3c53d5
Stevan Kriss <stevan.kriss@duke.edu>
2021-09-15 13:18:12 -0400

    fixed syntax of parantheses

--- test_Blood_Calculator.py
+++ test_Blood_Calculator.py
@@ -4,7 +4,7 @@ import pytest
     (65, "Normal"),
     (45, "Borderline Low"),
     (15, "Low"),
-    (70, "Normal"])
+    (70, "Normal")])
 def test_HDL_Anlysis(HDL_value, expected):
     from Blood_Calculator import HDL_Analysis
     answer = HDL_Analysis(HDL_value)
