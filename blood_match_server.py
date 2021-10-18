df0e87d3b1f634e99469c87a240e3ae29c10142d
Stevan Kriss <stevan.kriss@duke.edu>
2021-10-17 21:41:19 -0400

    Recommended Learning Project - Name Server

---
+++ blood_match_server.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+server_name = " http://vcm-7631.vm.duke.edu:5002/"
+
+r1 = requests.get(server_name + "get_patients/sak73")
+# print(r1.text)
+ID_dict = json.loads(r1.text)
+# print(ID_dict, type(ID_dict))
+ID1 = ID_dict["Donor"]
+ID2 = ID_dict["Recipient"]
+# print(ID1, ID2)
+
+r2 = requests.get(server_name + "get_blood_type/" + ID1)
+r3 = requests.get(server_name + "get_blood_type/" + ID2)
+bt_donor = r2.text
+bt_recipient = r3.text
+print(bt_donor, type(bt_donor))
+print(bt_recipient, type(bt_recipient))
+
+acceptable_match = []
+
+if bt_donor == "A+" and bt_recipient == "A+" or bt_recipient == "AB+":
+    acceptable_match = "Yes"
+elif bt_donor == "O+" and bt_recipient == "O+" or bt_recipient == "A+" or bt_recipient == "B+" or bt_recipient == "AB+":
+    acceptable_match = "Yes"
+elif bt_donor == "B+" and bt_recipient == "B+" or bt_recipient == "AB+":
+    acceptable_match = "Yes"
+elif bt_donor == "AB+" and bt_recipient == "AB+":
+    acceptable_match = "Yes"
+elif bt_donor == "A-" and bt_recipient == "A+" or bt_recipient == "A-" or bt_recipient == "AB+" or bt_recipient == "AB-":
+    acceptable_match = "Yes"
+elif bt_donor == "O-":
+    acceptable_match = "Yes"
+elif bt_donor == "B-" and bt_recipient == "B+" or bt_recipient == "B-" or bt_recipient == "AB+" or bt_recipient == "AB-":
+    acceptable_match = "Yes"
+elif bt_donor == "AB-" and bt_recipient == "AB+" or bt_recipient == "AB-":
+    acceptable_match = "Yes"
+else:
+    acceptable_match = "No"
+
+print(acceptable_match)
+
+request_json = {"Name": "sak73",
+                "Match": acceptable_match}
+
+r4 = requests.post(server_name + "match_check", json=request_json)
+print(r4.text)
--- post_request_exercise.py
+++ post_request_exercise.py
@@ -2,8 +2,17 @@ import requests
 
 server_name = "http://vcm-21170.vm.duke.edu:5000/"
 
-request_json = {"name": "Stevan Kriss",
+request_json1 = {"name": "Stevan Kriss",
                  "net_id": "sak73",
                  "e-mail": "stevan.kriss@duke.edu"}
 
-r = requests.post(server_name + "student", json = request_json)
+request_json2 = {"a": 1,
+                 "b": 2}
+
+r1 = requests.post(server_name + "student", json=request_json1)
+r2 = requests.get(server_name + "list")
+r3 = requests.post(server_name + "sum", json=request_json2)
+
+print(r1.text)
+print(r2.text)
+print(r3.text)
